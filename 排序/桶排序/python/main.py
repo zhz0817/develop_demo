ea9401768a7e8504cfffe12023487e8cd6b73ac6
zhz0817 <zhz15114585761@gmail.com>
2024-03-06 16:32:00 +0800

    基数排序

--- 排序/桶排序/python/main.py
+++ 排序/桶排序/python/main.py
@@ -1,9 +1,7 @@
 
 if __name__ == '__main__':
     nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    max_value = -1
-    for n in nums:
-        max_value = max(max_value,n)
+    max_value = max(nums)
     temp = [-1]*(1+max_value)
     for n in nums:
         temp[n] = n
