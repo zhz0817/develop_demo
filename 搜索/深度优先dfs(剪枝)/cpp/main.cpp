531b9b3ba55e5e047cfd3bf13e59871d69757a00
root <zhz15114585761@gmail.com>
2024-03-06 21:48:44 +0800

    dfs and bfs

---
+++ 搜索/深度优先dfs(剪枝)/cpp/main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+// https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int ans = INT32_MAX;
+    void dfs(TreeNode *root, int depth)
+    {
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            ans = std::min(ans, depth);
+            return;
+        }
+        if (depth >= ans)
+        {
+            return;
+        }
+        if (root->left != nullptr)
+        {
+            dfs(root->left, depth + 1);
+        }
+        if (root->right != nullptr)
+        {
+            dfs(root->right, depth + 1);
+        }
+    }
+    int minDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        dfs(root, 1);
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
