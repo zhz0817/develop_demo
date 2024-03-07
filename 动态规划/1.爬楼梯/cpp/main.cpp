2cb2df55d0e939662721aa70f443f7db0c04ab76
root <zhz15114585761@gmail.com>
2024-03-07 23:45:04 +0800

    爬楼梯

--- 动态规划/1.爬楼梯/cpp/main.cpp
+++ 动态规划/1.爬楼梯/cpp/main.cpp
@@ -1,29 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
-// https://leetcode.cn/problems/add-strings/
-
+// https://leetcode.cn/problems/climbing-stairs/description/
 class Solution {
 public:
-  string addStrings(string num1, string num2) {
-    int i = num1.length() - 1, j = num2.length() - 1, add = 0;
-    string ans = "";
-    while (i >= 0 || j >= 0 || add != 0) {
-      int x = i >= 0 ? num1[i] - '0' : 0;
-      int y = j >= 0 ? num2[j] - '0' : 0;
-      int result = x + y + add;
-      ans.push_back('0' + result % 10);
-      add = result / 10;
-      i -= 1;
-      j -= 1;
+  int climbStairs(int n) {
+    if (n == 1)
+      return 1;
+    if (n == 2) {
+      return 2;
     }
-    // 计算完以后的答案需要翻转过来
-    reverse(ans.begin(), ans.end());
-    return ans;
-    //
-    //        作者：力扣官方题解
-    //        链接：https://leetcode.cn/problems/add-strings/solutions/357938/zi-fu-chuan-xiang-jia-by-leetcode-solution/
-    //        来源：力扣（LeetCode）
-    //        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    int dp[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; ++i) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
   }
 };
 int main() { return 0; }
--- 动态规划/1.爬楼梯/python/main.py
+++ 动态规划/1.爬楼梯/python/main.py
@@ -1,35 +1,25 @@
-from functools import cmp_to_key
-from typing import List
+from typing import List, Optional
 
 
-# import sys
-#
-# sys.setrecursionlimit(100000)  # 例如这里设置为十万
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
 
-    def addStrings(self, num1: str, num2: str) -> str:
-        i = len(num1) - 1
-        j = len(num2) - 1
-        ans = []
-        add = 0
-        while i >= 0 or j >= 0 or add != 0:
-            x = 0
-            y = 0
-            if i >= 0:
-                x = int(num1[i])
-            if j >= 0:
-                y = int(num2[j])
-            result = x + y + add
-            ans.append(str(result % 10))
-            add = result // 10
-            i -= 1
-            j -= 1
-        ans.reverse()
-        return "".join(ans)
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
 
 
 if __name__ == '__main__':
     s = Solution()
-    s.addStrings("11","123")
