515c2e33d2bf30478c88f83e155166a934e1c8d9
zhz0817 <zhz15114585761@gmail.com>
2024-03-11 16:38:20 +0800

    排序讲课内容修改

--- 排序/堆排序/cpp/main.cpp
+++ 排序/堆排序/cpp/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    std::vector<int> nums{10,9,8,7,6,5,4,3,2,1};//简单介绍一下vector
+    std::vector<int> nums{10,9,8,7,6,5,4,3,2,1};
     priority_queue<int,vector<int>,greater<int>> pq;
     for(int& n:nums){
         pq.push(n);
--- 排序/快速排序/cpp/main.cpp
+++ 排序/快速排序/cpp/main.cpp
@@ -17,32 +17,32 @@ int partition(vector<int>& nums,int left,int right){
     int l=left;//左指针
     int r=right;//右指针
 
-    while(l!=r){
+    while(l<r){
         //控制右指针向左移动，找到小于基准元素的那个数
         while((l<r)&&(nums[r]>p)){
             r--;
         }
+        if(l<r){
+            nums[l++] = nums[r];
+        } 
         //控制左指针向右移动，找到大于基准元素的那个数
         while((l<r)&&(nums[l]<=p)){
             l++;
         }
-        //交换l指针和r指针所指的元素
         if(l<r){
-            int tmp=nums[l];
-            nums[l]=nums[r];
-            nums[r]=tmp;
+            nums[r--] = nums[l];
         }
     }
     //交换基准元素和重合点的元素
-    nums[left]=nums[l];
     nums[l]=p;
     return l;
 }
 int main(){
-    std::vector<int> nums{10,9,8,7,6,5,4,3,2,1};//简单介绍一下vector
+    std::vector<int> nums{10,9,8,7,6,5,4,3,2,1};
     quick_sort(nums,0,(int)nums.size()-1);
     for(int& n:nums){
         cout<<n<<endl;
     }
+    system("pause");
     return 0;
 }
