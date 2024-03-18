a6bfdfb45060d3c48972bb4e8c4061a7ef912baa
zhz0817 <zhz15114585761@gmail.com>
2024-03-18 11:45:36 +0800

    大数运算api

--- 模拟/大数加法/cpp/main.cpp
+++ 模拟/大数加法/cpp/main.cpp
@@ -19,11 +19,6 @@ public:
     // 计算完以后的答案需要翻转过来
     reverse(ans.begin(), ans.end());
     return ans;
-    //
-    //        作者：力扣官方题解
-    //        链接：https://leetcode.cn/problems/add-strings/solutions/357938/zi-fu-chuan-xiang-jia-by-leetcode-solution/
-    //        来源：力扣（LeetCode）
-    //        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
   }
 };
 int main() { return 0; }
