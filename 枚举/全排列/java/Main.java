d51a75f025afa80c0712b9f0723e7a142e9d64c5
zhz0817 <zhz15114585761@gmail.com>
2024-03-22 11:22:00 +0800

    备课

--- 枚举/全排列/java/Main.java
+++ 枚举/全排列/java/Main.java
@@ -30,4 +30,5 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
     }
+// https://leetcode.cn/problems/permutations/solutions/218275/quan-pai-lie-by-leetcode-solution-2/
 }
